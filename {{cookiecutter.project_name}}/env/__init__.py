98eed0fe4fc8ef4db5c37a04c565232e40165526
bjahnke <bjahnke71@gmail.com>
2023-06-09 12:56:09 -0400

    updated auto_env code with description

--- {{cookiecutter.project_name}}/env/__init__.py
+++ {{cookiecutter.project_name}}/env/__init__.py
@@ -1,2 +1,7 @@
 # env_auto.py is generated from .env by the `invoke buildenvpy` task.
-from env.env_auto import *
+try:
+    from env.env_auto import *
+except ModuleNotFoundError as e:
+    # print("env/env_auto.py not found.  Run tasks.py buildenvpy to build it from .env")
+    e.msg = "env/env_auto.py not found.  Run `invoke buildenvpy` to build it from .env"
+    raise e
