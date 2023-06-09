c6f9a45096776af76506e55a0cb80789bee88ec1
bjahnke <bjahnke71@gmail.com>
2023-06-09 12:56:48 -0400

    removed comment

--- {{cookiecutter.project_name}}/env/__init__.py
+++ {{cookiecutter.project_name}}/env/__init__.py
@@ -2,6 +2,5 @@
 try:
     from env.env_auto import *
 except ModuleNotFoundError as e:
-    # print("env/env_auto.py not found.  Run tasks.py buildenvpy to build it from .env")
     e.msg = "env/env_auto.py not found.  Run `invoke buildenvpy` to build it from .env"
     raise e
