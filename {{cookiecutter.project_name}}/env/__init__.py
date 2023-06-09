bc290c67461a6e2e5310a03824723ff1dabeccc3
bjahnke <bjahnke71@gmail.com>
2023-06-09 13:01:34 -0400

    import env_auto prints message rather than throwing error

--- {{cookiecutter.project_name}}/env/__init__.py
+++ {{cookiecutter.project_name}}/env/__init__.py
@@ -2,5 +2,8 @@
 try:
     from env.env_auto import *
 except ModuleNotFoundError as e:
-    e.msg = "env/env_auto.py not found.  Run `invoke buildenvpy` to build it from .env"
-    raise e
+    print(
+        "env/env_auto.py not found. Module import skipped.\n"
+        "Run `invoke buildenvpy` to build it from .env \n"
+        "(ignore this message if triggered by running `invoke buildenvpy`)"
+    )
