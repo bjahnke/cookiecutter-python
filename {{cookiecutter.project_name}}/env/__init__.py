bb45b55530b67797627e55112c1fb1b21719a45d
bjahnke <bjahnke71@gmail.com>
2023-06-09 13:05:34 -0400

    empty auto_env.py included in template, removed try catch when not exists

--- {{cookiecutter.project_name}}/env/__init__.py
+++ {{cookiecutter.project_name}}/env/__init__.py
@@ -1,9 +1,1 @@
-# env_auto.py is generated from .env by the `invoke buildenvpy` task.
-try:
-    from env.env_auto import *
-except ModuleNotFoundError as e:
-    print(
-        "env/env_auto.py not found. Module import skipped.\n"
-        "Run `invoke buildenvpy` to build it from .env \n"
-        "(ignore this message if triggered by running `invoke buildenvpy`)"
-    )
+from env.env_auto import *
